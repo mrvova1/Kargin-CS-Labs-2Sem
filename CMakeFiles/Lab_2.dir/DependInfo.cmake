109ab27bd33dea47999ee3507c0a981e48a89594
Mrvova1 <Vova.kargin.06@mail.ru>
2025-02-21 22:04:13 +0300

    main.cpp compleate

---
+++ CMakeFiles/Lab_2.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mrvova/Kargin-CS-Labs-2Sem/main.cpp" "CMakeFiles/Lab_2.dir/main.cpp.o" "gcc" "CMakeFiles/Lab_2.dir/main.cpp.o.d"
+  "/home/mrvova/Kargin-CS-Labs-2Sem/planets.cpp" "CMakeFiles/Lab_2.dir/planets.cpp.o" "gcc" "CMakeFiles/Lab_2.dir/planets.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Lab_2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_2.dir/main.cpp.o"
+  "CMakeFiles/Lab_2.dir/main.cpp.o.d"
+  "CMakeFiles/Lab_2.dir/planets.cpp.o"
+  "CMakeFiles/Lab_2.dir/planets.cpp.o.d"
+  "Lab_2"
+  "Lab_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
