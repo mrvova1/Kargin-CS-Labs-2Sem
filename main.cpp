a68e08b0b5114827b1795b50661fbcf7bf788e97
Mrvova1 <Vova.kargin.06@mail.ru>
2025-03-25 16:08:13 +0300

    main.cpp compleate

--- main.cpp
+++ main.cpp
@@ -1,85 +1,130 @@
 #include <iostream>
-#include "planets.h"
 #include <cstring>
+#include "fractions.h"
 
-class
-int main(int argc, char* argv[]) {
-    const int INITIAL_CAPACITY = 120;
-    const int MAX_NAME_LENGTH = 100;
-    char file_name[MAX_NAME_LENGTH];
-
-    // Динамический массив планет
-    Planet* planets = new Planet[INITIAL_CAPACITY];
-    int capacity = INITIAL_CAPACITY;
-    int n_planet = 0;
-    int ind;
-
-    if (argc > 1 && strcmp(argv[1], "i") == 0) {
-        while (true) {
-            MenuOption choice = static_cast<MenuOption>(Planet::menu());
-            switch (choice) {
-                case READ_FROM_FILE:
-                    std::cout << "Введите название файла" << '\n';
-                    std::cin >> file_name;
-                    n_planet = Planet::read_db(file_name, planets, n_planet, capacity);
-                    break;
-                case WRITE_TO_FILE:
-                    std::cout << "Введите название файла" << '\n';
-                    std::cin >> file_name;
-                    Planet::write_db(file_name, planets, n_planet);
-                    break;
-                case EDIT_PLANET:
-                    if ((ind = Planet::find(planets, n_planet)) >= 0)
-                        std::cin >> planets[ind];
-                    else
-                        std::cout << "Такой планеты нет" << std::endl;
-                    break;
-                case PRINT_PLANETS:
-                    Planet::print_db(planets, n_planet);
-                    break;
-                case SORT_BY_DIAMETER:
-                    Planet::sort_db(planets, n_planet);
-                    break;
-                case SORT_BY_NAME:
-                    Planet::sort_by_name(planets, n_planet);
-                    break;
-                case ADD_PLANET:
-                    Planet::add_planet(planets, n_planet, capacity);
-                    break;
-                case DELETE_PLANET:
-                    if ((ind = Planet::find(planets, n_planet)) >= 0)
-                        Planet::delete_planet(planets, n_planet, ind);
-                    else
-                        std::cout << "Такой планеты нет" << std::endl;
-                    break;
-                case EXIT_PROGRAM:
-                    delete[] planets;
-                    return 0;
-                default:
-                    std::cout << "Неправильный ввод" << std::endl;
-                    break;
+int interactive() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Интерактивный режим.\n";
+    while (true) {
+        MenuOption choice = static_cast<MenuOption>(Fractions::menu());
+        switch (choice) {
+            case INPUT_FRACTION: {
+                std::cin.ignore();
+                Fractions f;
+                std::cout << "Введите дробь:" << std::endl;
+                std::cin >> f;
+                std::cout << "Введённая дробь: " << f << std::endl;
+                break;
             }
+            case ADD_FRACTIONS: {
+                std::cin.ignore();
+                Fractions a, b;
+                std::cout << "Введите первую дробь:" << std::endl;
+                std::cin >> a;
+                std::cout << "Введите вторую дробь:" << std::endl;
+                std::cin >> b;
+                Fractions res = a + b;
+                std::cout << "Результат сложения: " << res << std::endl;
+                break;
+            }
+            case SUBTRACT_FRACTIONS: {
+                std::cin.ignore();
+                Fractions a, b;
+                std::cout << "Введите первую дробь:" << std::endl;
+                std::cin >> a;
+                std::cout << "Введите вторую дробь:" << std::endl;
+                std::cin >> b;
+                Fractions res = a - b;
+                std::cout << "Результат вычитания: " << res << std::endl;
+                break;
+            }
+            case MULTIPLY_FRACTIONS: {
+                std::cin.ignore();
+                Fractions a, b;
+                std::cout << "Введите первую дробь:" << std::endl;
+                std::cin >> a;
+                std::cout << "Введите вторую дробь:" << std::endl;
+                std::cin >> b;
+                Fractions res = a * b;
+                std::cout << "Результат умножения: " << res << std::endl;
+                break;
+            }
+            case DIVIDE_FRACTIONS: {
+                std::cin.ignore();
+                Fractions a, b;
+                std::cout << "Введите первую дробь:" << std::endl;
+                std::cin >> a;
+                std::cout << "Введите вторую дробь:" << std::endl;
+                std::cin >> b;
+                Fractions res = a / b;
+                std::cout << "Результат деления: " << res << std::endl;
+                break;
+            }
+            case EXIT_PROGRAM: {
+                return 0;
+            }
+            default:
+                std::cout << "Неправильный ввод" << std::endl;
+                break;
         }
-    } else if (argc > 1 && strcmp(argv[1], "d") == 0) {
-        std::strcpy(file_name, "Sun.txt");
-        n_planet = Planet::read_db(file_name, planets, n_planet, capacity);
-        Planet::print_db(planets, n_planet);
-        std::cout << '\n';
-
-        std::cin >> planets[2];
-        std::cout << planets[2] << std::endl;
-        Planet::sort_by_name(planets, n_planet);
-        Planet::print_db(planets, n_planet);
-        std::cout << '\n';
-
-        Planet::sort_db(planets, n_planet);
-        Planet::print_db(planets, n_planet);
-        std::cout << '\n';
-
-        Planet::add_planet(planets, n_planet, capacity);
-        Planet::delete_planet(planets, n_planet, 2);
-        Planet::print_db(planets, n_planet);
     }
-    delete[] planets;
     return 0;
+}
+
+int demo() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Демо режим\n";
+    // ввод дроби с клавиатуры
+    std::cout << "Введите дробь: \n";
+    Fractions z;
+    std::cin >> z;
+    std::cout << "z=" << z << std::endl;
+    // проверка конструкторов
+    Fractions fr1(0, 10, 14), fr2;
+    std::cout << "fr2=" << fr2 << std::endl;
+    std::cout << "fr1=" << fr1 << std::endl;
+    Fractions fr = "-1 4/8";
+    std::cout << "fr=" << fr << std::endl;
+    Fractions x(z), y;
+    std::cout << "x=" << x << std::endl;
+    double dbl = -1.25;
+    Fractions f = dbl;
+    std::cout << "f=" << f << std::endl;
+    // проверка перегруженной операции "+"
+    y = x + z;
+    std::cout << "y=" << y << std::endl;
+    y += x;
+    f += dbl / 2.0;
+    std::cout << "f=" << f << std::endl;
+    y = x + dbl;
+    std::cout << "y=" << y << std::endl;
+    y = dbl + y;
+    std::cout << "y=" << y << std::endl;
+    y += dbl;
+    std::cout << "y=" << y << std::endl;
+    int i = 5;
+    y += i;
+    std::cout << "y=" << y << std::endl;
+    y = i + x;
+    std::cout << "y=" << y << std::endl;
+    y = x + i;
+    std::cout << "y=" << y << std::endl;
+    y += dbl + i + x;
+    std::cout << "y=" << y << std::endl;
+    y = dbl - i - x;
+    std::cout << "y=" << y << std::endl;
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    bool isInteractive = false;
+    if (argc == 2 && std::strcmp(argv[1], "i") == 0) {
+        isInteractive = true;
+    }
+    if (isInteractive) {
+        return interactive();
+    } else {
+        return demo();
+    }
 }
