0b6d8e697eb441c74b632f365c8724250f527d2f
Mrvova1 <Vova.kargin.06@mail.ru>
2025-05-19 16:35:54 +0300

    main.cpp compleate

--- engine.h
+++ engine.h
@@ -4,16 +4,26 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <vector>
+#include <list>
+#include <map>
+#include <random>
 
 int s_count(std::string &first_s, std::string &second_s){
     int count = 0;
     char last_s = second_s[second_s.length()-1];
-    for (size_t i = 0; i < first_s.length(); i++){
-        if (first_s[i] == last_s) {
-            count++;
-        }
+    while (first_s.find(last_s, count) != std::string::npos)
+    {
+        count++;
+        // std::cout <<
     }
+
+    // for (size_t i = 0; i < first_s.length(); i++){
+    //     if (first_s[i] == last_s) {
+    //         count++;
+    //     }
+    // }
     return count;
 }
 
@@ -27,20 +37,32 @@ int first(){
 
 std::string s_info(std::string &first_s){
     std::string info;
-    bool start = false;
-    bool end = false;
-    char c;
-    for (size_t i = 0; i < first_s.length(); i++){
-        c = first_s[i];
-        if (c == ',') {
-            end = true;
-        }
-        if (end) {break;}
-        if (start) {info.push_back(c);}
-        if (c == ' ') {
-            start = true;
-        }
-    }
+    size_t space = first_s.find(' ');
+    size_t comma = first_s.find(',');
+
+    if (space == std::string::npos){ std::cout << "Нет пробела\n"; return "";}
+    if (comma == std::string::npos){ std::cout << "Нет запятой\n"; return "";}
+    if (space >= comma){ return "";}
+
+
+    return first_s.substr(space+1, comma-space);
+
+    // bool start = false;
+    // bool end = false;
+    // char c;
+
+
+    // for (size_t i = 0; i < first_s.length(); i++){
+    //     c = first_s[i];
+    //     if (c == ',') {
+    //         end = true;
+    //     }
+    //     if (end) {break;}
+    //     if (start) {info.push_back(c);}
+    //     if (c == ' ') {
+    //         start = true;
+    //     }
+    // }
     return info;
 }
 
@@ -82,8 +104,8 @@ std::vector<float> vector_summ(std::vector<float> &first_v, std::vector<float> &
 }
 
 void fourth_print(std::vector<float> vector){
-    for (size_t i=0; i < vector.size(); i++){
-        std::cout << vector[i] << ' ';
+    for (float i : vector){
+        std::cout << i << ' ';
     }
     std::cout << '\n';
 }
@@ -92,11 +114,12 @@ void fourth(){
     std::vector<float> first_v;
     std::vector<float> second_v;
     std::vector<float> third_v;
-    float number = 1.0;
+    float randomNum;
     for (int i=0; i < 25; i++){
-        number *= static_cast<float>(i) + 0.1;
-        first_v.push_back(number);
-        second_v.push_back(number*0.52);
+        randomNum = (rand() % 10001) / 100;
+        first_v.push_back(randomNum);
+        randomNum = (rand() % 10001) / 100;
+        second_v.push_back(randomNum);
     }
     third_v = vector_summ(first_v, second_v);
     fourth_print(first_v);
@@ -118,35 +141,188 @@ void fivth_print(std::vector<Planet> vector){
     }
 }
 
+int plus_count(std::list<int> list_n){
+    std::list<int>::iterator iter;
+    int count = 0;
+    for (iter = list_n.begin(); iter != list_n.end(); iter++) {
+        if (*iter > 0) {count++;};
+    }
+    return count;
+}
+
+void sixth(){
+    std::list<int> list_n;
+    std::list<int>::iterator iter;
+    int randomNum;
+    for (int i=0; i<10; i++){
+        randomNum = rand() % 1000 - 500;
+        list_n.push_back(randomNum);
+    }
+    for (iter = list_n.begin(); iter != list_n.end(); iter++) {
+        std::cout << *iter << ' ';
+    }
+    std::cout << '\n';
+    std::cout << plus_count(list_n) << '\n';
+}
+
+class Card {
+  std::string title;   // заглавие книги
+  std::string author;  // автор
+  int number;     // количество имеющихся экземпляров
+ public:
+  Card() : title(""), author(""), number(0) {}
+  Card(std::string t, std::string a, int n) : title(t), author(a), number(n) {}
+  std::string get_title(){return title;}
+  std::string get_author(){return author;}
+  int get_number(){return number;}
+};
+
+void seventh() {
+    std::list<Card> list_cards;
+    list_cards.push_back(Card("Невероятные приключения ДжоДжо часть 1", "Хирохико Араки", 9));
+    list_cards.push_back(Card("Невероятные приключения ДжоДжо часть 2", "Хирохико Араки", 9));
+    list_cards.push_back(Card("Невероятные приключения ДжоДжо часть 3", "Хирохико Араки", 9));
+    list_cards.push_back(Card("Магическая битва", "Геге Акутами", 3));
+    list_cards.push_back(Card("Что-то", "Кто-то", 100));
+    list_cards.push_back(Card("Яблоки", "Никита", 54321));
+    list_cards.push_back(Card("Почему", "Зачем", 1));
+    std::list<Card>::iterator iter;
+    for (iter = list_cards.begin(); iter != list_cards.end(); iter++) {
+      std::cout << iter->get_title() << " - " << iter->get_author() << ' ' << iter->get_number() << "шт" << std::endl;
+    }
+    std::string author;
+    std::cout << "Введите автора" << std::endl;
+    std::getline(std::cin, author);
+    bool in_l = false;
+    for (iter = list_cards.begin(); iter != list_cards.end(); iter++) {
+        // std::cout << iter->get_author();
+        if (iter->get_author().compare(author) == 0){ std::cout << iter->get_title() << ' ' << iter->get_number() << "шт" << std::endl; in_l=true;}
+    }
+    if (not in_l){
+        std::cout << "Книги данного автора отсутствуют" << std::endl;
+    }
+}
+
+void eighth() {
+    std::map<int, double> m;
+    std::map<int, double>::iterator i;
+    for (int j=0; j<10; j++){
+        m[j] = static_cast<float>(j) + static_cast<float>(j)/10;
+    }
+    for (i = m.begin(); i != m.end(); i++){
+        std::cout << i->first << " -> " << i->second << "; \n";
+    }
+
+    for (i = m.begin(); i != m.end();){
+        if (i->first % 2 == 0) {
+            auto j = i++;
+            m.erase(j->first);
+        }
+        else {
+            i++;
+        }
+    }
+    std::cout << '\n';
+    for (i = m.begin(); i != m.end(); i++){
+        std::cout << i->first << " -> " << i->second << "; \n";
+    }
+}
+
+void ninth(){
+    std::map<std::string, std::string> m;
+    std::map<std::string, std::string>::iterator i;
+    std::string number;
+    std::string name;
+    std::cout << "Справочник номеров\n";
+    while (true)
+    {
+        std::cout << "Введите номер (чтобы выйти введите 0)\n";
+        std::getline(std::cin, number);
+        if (number.compare("0") == 0) {break;}
+        std::cout << "Введите фамилию\n";
+        std::getline(std::cin, name);
+        m[number] = name;
+    }
+
+    // m["5671234"] = "Ivanov";
+    // m["3214567"] = "Petrov";
+    // m["9871234"] = "Sidorov";
+
+    // std::string mas[4] = { "1112233", "9871234", "5671234", "5556688" };
+
+    std::string mas[100];
+    size_t mas_i= 0;
+    std::cout << "Введите номера людей\n";
+    while (true)
+    {
+        std::cout << "Введите номер (чтобы выйти введите 0)\n";
+        std::getline(std::cin, number);
+        if (number.compare("0") == 0) {break;}
+        // mas->append(number);
+        mas[mas_i] = number;
+        mas_i++;
+    }
+    // std::cout << mas->size() << ";;;; \n";
+    for (size_t j=0; j<mas_i;j++){
+        for (i = m.begin(); i != m.end(); i++){
+            // std::cout << i->first << " -> " << i->second << "; \n";
+            // std::cout << i->first << '\n';
+            if (i->first.compare(mas[j]) == 0) std::cout << i->second << "\n";
+        }
+        // std::cout << mas[j] << "\n\n";
+    }
+
+}
+
 int main(){
-    // std::cout << first();
-    // std::cout << second() << '\n';
-    // third();
-    // fourth();
+    std::cout << "\nЗадача 1\n";
+    std::cout << "Введите 2 слова\n";
+    std::cout << first();
 
-    // std::vector<Planet> planets;
-    // planets.push_back(Planet("Меркурий", 0));
-    // planets.push_back(Planet("Венера", 0));
-    // planets.push_back(Planet("Земля", 1));
-    // planets.push_back(Planet("Марс", 2));
-    // planets.push_back(Planet("Юпитер", 69));
-    // planets.push_back(Planet("Сатурн", 62));
-    // planets.push_back(Planet("Уран", 27));
-    // planets.push_back(Planet("Нептун", 14));
-    // planets.push_back(Planet("Плутон", 10));
-    // fivth_print(planets);
-    // std::cout << '\n';
-    // int max_sat = -1;
-    // Planet max_plan = Planet();
-    // for (size_t i=0; i < planets.size(); i++){
-    //     if (planets[i].satelites_) {
-    //         max_sat = planets[i].satelites_;
-    //         max_plan = planets[i];
-    //     }
-    // }
-    // std::cout << max_plan.name_ << max_plan.satelites_ <<  '\n';
+    std::cout << "\nЗадача 2\n";
+    std::cout << "Введите слово\n";
+    std::cout << second() << '\n';
 
+    std::cout << "\nЗадача 3\n";
+    third();
 
+    std::cout << "\nЗадача 4\n";
+    fourth();
+
+    std::cout << "\nЗадача 5\n";
+    std::vector<Planet> planets;
+    planets.emplace_back("Меркурий", 0);
+    planets.emplace_back("Венера", 0);
+    planets.emplace_back("Земля", 1);
+    planets.emplace_back("Марс", 2);
+    planets.emplace_back("Юпитер", 69);
+    planets.emplace_back("Сатурн", 62);
+    planets.emplace_back("Уран", 27);
+    planets.emplace_back("Нептун", 14);
+    planets.emplace_back("Плутон", 10);
+    fivth_print(planets);
+    std::cout << '\n';
+    int max_sat = -1;
+    Planet max_plan = Planet();
+    for (size_t i=0; i < planets.size(); i++){
+        if (planets[i].satelites_ > max_sat) {
+            max_sat = planets[i].satelites_;
+            max_plan = planets[i];
+        }
+    }
+    std::cout << max_plan.name_ << max_plan.satelites_ <<  '\n';
+
+    std::cout << "\nЗадача 6\n";
+    sixth();
+
+    std::cout << "\nЗадача 7\n";
+    seventh();
+
+    std::cout << "\nЗадача 8\n";
+    eighth();
+
+    std::cout << "\nЗадача 9\n";
+    ninth();
 
     return 0;
 }
