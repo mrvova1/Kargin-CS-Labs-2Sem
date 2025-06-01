0e9976360a87aa1a985ca89a4cf8890d77504277
Mrvova1 <Vova.kargin.06@mail.ru>
2025-06-01 20:38:33 +0300

    main.cpp compleate

--- MySet.h
+++ MySet.h
@@ -1,384 +1,373 @@
-// MySet.h
-#ifndef MYSET_H
-#define MYSET_H
-
-#include <iostream>
+#pragma once
 #include <cstring>
+#include <iostream>
 
-template<typename INF>
+template<class INF>
 class MyVector {
-private:
-    size_t max_size_;
-    size_t size_;
+ protected:
+    int max_size_;
+    int size_;
     INF* pdata_;
-public:
-    MyVector(size_t max_size = 1);
+    void resize();
+
+ public:
+    MyVector(INF el = NULL, int max_size = 1);
     MyVector(const MyVector& other);
-    MyVector& operator=(const MyVector& other);
     ~MyVector();
 
-    void add_element(INF new_element);
-    int delete_element(size_t index);
-    int find(INF el) const;
-    void is_resize();
-    void resize(size_t new_size);
+    void add_element(INF el);
+    bool delete_element(int index);
     void sort();
+    int find(INF el);
 
-    INF operator[](size_t num) const;
-    size_t len() const;
-    template<typename I>
-    friend std::ostream& operator<<(std::ostream& os, const MyVector<I>& St);
+    MyVector<INF>& operator=(const MyVector<INF>& other);
+    const INF& operator[](int index) const;
+    template <class I>
+    friend std::ostream& operator<<(std::ostream& out, MyVector<I>& other);
 };
 
-template<>
-int MyVector<char*>::find(char* el) const;
-
-template<>
-MyVector<char*>::~MyVector();
-
-template<>
-void MyVector<char*>::add_element(char* new_element);
-
-template<>
-int MyVector<char*>::delete_element(size_t index);
-
-
-template<typename INF>
-std::ostream& operator<<(std::ostream& os, const MyVector<INF>& St) {
-    for (size_t i = 0; i < St.len(); ++i) {
-        std::cout << St[i] << ' ';
+template<class INF>
+void MyVector<INF>::resize() {
+    max_size_ *= 2;
+    INF* newSize = new INF[max_size_];
+    for (int i = 0; i < size_; ++i) {
+        newSize[i] = pdata_[i];
     }
-    std::cout << std::endl;
-    return os;
+    delete[] pdata_;
+    pdata_ = newSize;
 }
 
-template<typename INF>
-class MySet : public MyVector<INF> {
-public:
-    bool is_element(INF el) const;
-    bool add_element(INF el);
-    bool delete_element(INF el);
+template<class INF>
+MyVector<INF>::MyVector(INF el , int max_size) {
+    max_size_ = max_size;
+    size_ = 0;
+    pdata_ = new INF[max_size_];
+    if (el) {
+        add_element(el);
+    }
+}
 
-    void operator+=(const MySet& St);
-    void operator-=(const MySet& St);
-    void operator*=(const MySet& St);
-
-    template<class I>
-    friend std::ostream& operator<<(std::ostream& os, const MySet<I>& St);
-
-    template<typename I>
-    friend MySet<I> operator*(const MySet<I>& f_st, const MySet<I>& s_st);
-    template<typename I>
-    friend MySet<I> operator+(const MySet<I>& f_st, const MySet<I>& s_st);
-    template<typename I>
-    friend MySet<I> operator-(const MySet<I>& f_st, const MySet<I>& s_st);
-    template<typename I>
-    friend bool operator==(const MySet<I>& f_st, const MySet<I>& s_st);
-};
-
-template<typename INF>
-std::ostream& operator<<(std::ostream& os, const MySet<INF>& St);
-
-template<typename INF>
-MyVector<INF>::MyVector(size_t max_size)
-    : max_size_(max_size), size_(0), pdata_(new INF[max_size]) {}
-
-template<typename INF>
-MyVector<INF>::MyVector(const MyVector& other)
-    : max_size_(other.max_size_), size_(other.size_), pdata_(new INF[other.max_size_]) {
-    for (size_t i = 0; i < max_size_; ++i) {
+template<class INF>
+MyVector<INF>::MyVector(const MyVector& other) {
+    max_size_ = other.max_size_;
+    size_ = other.size_;
+    pdata_ = new INF[max_size_];
+    for (int i = 0; i < size_; ++i) {
         pdata_[i] = other.pdata_[i];
     }
 }
 
-template<typename INF>
-MyVector<INF>& MyVector<INF>::operator=(const MyVector& other) {
-    if (this != &other) {
-        max_size_ = other.max_size_;
-        size_ = other.size_;
-        delete[] pdata_;
-        pdata_ = new INF[max_size_];
-        for (size_t i = 0; i < max_size_; ++i) {
-            pdata_[i] = other.pdata_[i];
-        }
-    }
-    return *this;
-}
-
 template<>
-MyVector<char*>::MyVector(const MyVector<char*>& other)
-    : max_size_(other.max_size_), size_(other.size_), pdata_(new char*[other.max_size_])
-{
-    for (size_t i = 0; i < size_; ++i) {
-        size_t len = std::strlen(other.pdata_[i]) + 1;
-        pdata_[i] = new char[len];
-        std::strcpy(pdata_[i], other.pdata_[i]);
+MyVector<char*>::MyVector(const MyVector& other) {
+    max_size_ = other.max_size_;
+    size_ = other.size_;
+    pdata_ = new char*[max_size_];
+    for (int i = 0; i < size_; ++i) {
+        pdata_[i]=new char[strlen(other[i])+1];
+        strcpy(pdata_[i],other[i]);
     }
 }
 
-template<>
-MyVector<char*>& MyVector<char*>::operator=(const MyVector<char*>& other) {
-    if (this != &other) {
-        for (size_t i = 0; i < size_; ++i) {
-            delete[] pdata_[i];
-        }
-        max_size_ = other.max_size_;
-        size_ = other.size_;
-        delete[] pdata_;
-        pdata_ = new char*[max_size_];
-        for (size_t i = 0; i < size_; ++i) {
-            // size_t len = std::strlen(other.pdata_[i]) + 1;
-            // char* copy = new char[len];
-            std::strcpy(pdata_[i], other.pdata_[i]);
-            // pdata_[i] = copy;
-        }
-    }
-    return *this;
-}
-
-template<typename INF>
+template<class INF>
 MyVector<INF>::~MyVector() {
     delete[] pdata_;
 }
 
 template<>
 MyVector<char*>::~MyVector() {
-    for (size_t i = 0; i < size_; ++i) {
+    for(int i =0;i<max_size_;++i){
         delete[] pdata_[i];
     }
     delete[] pdata_;
 }
 
-template<typename INF>
-void MyVector<INF>::add_element(INF new_element) {
+template<class INF>
+void MyVector<INF>::add_element(INF el) {
+    if (size_ >= max_size_) {
+        resize();
+    }
+    pdata_[size_] = el;
     ++size_;
-    if (size_ >= max_size_ / 2) {
-        is_resize();
-    }
-    pdata_[size_ - 1] = new_element;
 }
 
-template<typename INF>
-int MyVector<INF>::delete_element(size_t index) {
-    std::cout << '\n' << index << "index\n";
-    if (index >= size_) {
-        std::cout << "Индекс превосходит размер массива";
-        return -1;
+template<>
+void MyVector<char*>::add_element(char* el) {
+    if (size_ >= max_size_) {
+        resize();
     }
-    for (size_t i = index + 1; i < max_size_; ++i) {
-        pdata_[i - 1] = pdata_[i];
-    }
-    pdata_[size_ - 1] = 0;
-    --size_;
-    is_resize();
-    return 0;
+    pdata_[size_]=new char[strlen(el)+1];
+    strcpy(pdata_[size_],el);
+    ++size_;
 }
 
-template<typename INF>
-int MyVector<INF>::find(INF el) const {
-    if (size_ == 0) return -1;
-    int left_b = 0;
-    int right_b = size_ - 1;
-    int mid = (right_b - left_b) / 2 + left_b;
-    while (pdata_[mid] != el) {
-        if (pdata_[mid] > el)
-            right_b = mid - 1;
-        else
-            left_b = mid + 1;
-        mid = (right_b - left_b) / 2 + left_b;
-        if (right_b == left_b) {
-            if (pdata_[mid] == el) break;
-            return -1;
+template<class INF>
+bool MyVector<INF>::delete_element(int i) {
+    if (size_ == 0 || i < 0 || i >= size_) {
+        return false;
+    }
+    for (int j = 0; j < size_ - 1; ++j) {
+        if(j>=i) {
+            pdata_[j] = pdata_[j + 1];
         }
-        if (right_b < 0 || left_b > static_cast<int>(size_) - 1) return -1;
     }
-    return mid;
+    --size_;
+    if (size_ < max_size_ / 4) {
+        max_size_ = max_size_ / 2;
+    }
+    return true;
 }
 
-template<typename INF>
-void MyVector<INF>::is_resize(){
-    while (size_ > max_size_ / 2) {
-        resize(max_size_ * 2);
+template<>
+bool MyVector<char*>::delete_element(int i) {
+    if (size_ == 0 or i < 0 or i >= size_) {
+        return false;
     }
-    while (size_ < max_size_ / 4) {
-        resize(max_size_ / 2);
+    for (int j = i; j < size_ - 1; ++j) {
+        delete[] pdata_[j];
+        pdata_[j] = pdata_[j+1];
     }
+    --size_;
+    if (size_ < max_size_ / 4) {
+        max_size_ = max_size_ / 2;
+    }
+    return true;
 }
 
-template<typename INF>
-void MyVector<INF>::resize(size_t new_size) {
-    INF* new_pdata = new INF[new_size];
-    for (size_t i = 0; i < size_; ++i) new_pdata[i] = pdata_[i];
-    delete[] pdata_;
-    pdata_ = new_pdata;
-    max_size_ = new_size;
-}
-
-template<typename INF>
+template<class INF>
 void MyVector<INF>::sort() {
-    for (size_t i = 0; i < size_ - 1; ++i) {
-        for (size_t j = i + 1; j < size_; ++j) {
-            if (pdata_[j] < pdata_[i]) {
-                INF tmp = pdata_[i];
-                pdata_[i] = pdata_[j];
-                pdata_[j] = tmp;
+    if (!pdata_) {
+        return;
+    }
+    for (int i = 0; i < size_; ++i) {
+        for (int j = 0; j < size_ - i - 1; ++j) {
+            if (pdata_[j] > pdata_[j + 1]) {
+                std::swap(pdata_[j], pdata_[j + 1]);
             }
         }
     }
 }
 
-template<typename INF>
-INF MyVector<INF>::operator[](size_t num) const {
-    return pdata_[num];
+template<>
+void MyVector<char*>::sort() {
+    if (!pdata_) {
+        return;
+    }
+    for (int i = 0; i < size_; ++i) {
+        for (int j = 0; j < size_ - i - 1; ++j) {
+            if (strcmp(pdata_[j + 1], pdata_[j]) < 0) {
+                std::swap(pdata_[j], pdata_[j + 1]);
+            }
+        }
+    }
 }
 
-template<typename INF>
-size_t MyVector<INF>::len() const {
-    return size_;
+template<class INF>
+int MyVector<INF>::find(INF el) {
+    int index = -1;
+    for (int i = 0; i < this->size_; ++i) {
+        if (pdata_[i] == el) {
+            index = i;
+        }
+    }
+    return index;
 }
 
 template<>
-int MyVector<char*>::find(char* el) const {
-    for (size_t i = 0; i < size_; ++i) {
-        std::cout<<pdata_[i] << '\n';
-        if (std::strcmp(pdata_[i], el) == 0) return i;
+int MyVector<char*>::find(char* el) {
+    int index = -1;
+    for (int i = 0; i < this->size_; ++i) {
+        if (!strcmp(pdata_[i], el)) {
+            index =  i;
+        }
     }
-    return -1;
+    return index;
 }
 
-// template<>
-// MyVector<char*>::~MyVector() {
-//     for (size_t i = 0; i < size_; ++i)
-//      delete[] pdata_[i];
-//     delete[] pdata_;
-// }
-
-// template<>
-// MyVector<const char*>::~MyVector() {
-//     for (size_t i = 0; i < size_; ++i) {
-//         std::cout << pdata_[i];
-//         delete[] pdata_[i];
-//     }
-//     delete[] pdata_;
-// }
-
-template<>
-void MyVector<char*>::add_element(char* new_element) {
-    ++size_;
-    if (size_ >= max_size_ / 2) is_resize();
-    pdata_[size_-1] = new char[std::strlen(new_element) + 1];
-    std::strcpy(pdata_[size_ - 1], new_element);
+template<class INF>
+MyVector<INF>& MyVector<INF>::operator=(const MyVector<INF>& other) {
+    if(this==&other){
+        return *this;
+    }
+    delete[] pdata_;
+    this->max_size_ = other.max_size_;
+    this->size_ = other.size_;
+    this->pdata_ = new INF[max_size_];
+    for (int i = 0; i < size_; ++i) {
+        this->pdata_[i] = other[i];
+    }
+    return *this;
 }
 
 template<>
-int MyVector<char*>::delete_element(size_t index) {
-    if (index >= size_) {
-        std::cout << "Индекс превосходит размер массива";
-        return -1;
+MyVector<char*>& MyVector<char*>::operator=(const MyVector<char*>& other) {
+    if(this==&other){
+        return *this;
     }
-    std::cout << find(pdata_[index]) << "||||||||||||||||||||||";
-    std::cout << "Смерть";
-    delete[] pdata_[index];
-    for (size_t i = index; i < size_; ++i) {
-        pdata_[i] = pdata_[i+1];
+    delete[] pdata_;
+    this->max_size_ = other.max_size_;
+    this->size_ = other.size_;
+    this->pdata_ = new char*[max_size_];
+    for (int i = 0; i < size_; ++i) {
+        delete[] this->pdata_[i];
+        this->pdata_[i]=new char[strlen(other[i])+1];
+        strcpy(this->pdata_[i],other[i]);
     }
-    --size_;
-    is_resize();
-    return 0;
+    return *this;
 }
 
-template<typename INF>
-bool MySet<INF>::is_element(INF el) const {
-    if (this->len() == 0) return false;
-    return this->find(el) != -1;
+template<class INF>
+const INF& MyVector<INF>::operator[](int i) const {
+    if (i < 0 || i >= size_) {
+        std::cout<<"Введены некорректные данные";
+        return pdata_[max_size_];
+    }
+    return pdata_[i];
 }
 
-template<typename INF>
-bool MySet<INF>::add_element(INF el) {
-    if (!is_element(el)) {
-        this->MyVector<INF>::add_element(el);
+template<class I>
+std::ostream& operator<<(std::ostream& out, MyVector<I>& other) {
+    for (int i = 0; i < other.size_; ++i) {
+        out << other[i] << ' ';
+    }
+    return out;
+}
+
+template<class INF=const char*>
+class MySet : public MyVector<INF> {
+ public:
+    MySet(INF el = NULL) : MyVector<INF>(el) {}
+
+    void add_element(INF el);
+    void delete_element(INF el);
+    bool is_element(INF el) const;
+
+    template <class I>
+    friend std::ostream& operator<<(std::ostream& out, const MySet<I>& Set);
+    template <class I>
+    friend bool operator==(const MySet<I>& f_st,const MySet<I>& s_st);
+    template <class I>
+    friend MySet<I>& operator+=( MySet<I>& f_st,const MySet<I>& s_st);
+    template <class I>
+    friend MySet<I>& operator-=( MySet<I>& f_st,const MySet<I>& s_st);
+    template <class I>
+    friend MySet<I>& operator*=( MySet<I>& f_st,const MySet<I>& s_st);
+    template <class I>
+    friend MySet<I> operator+(const MySet<I>& f_st, const MySet<I>& s_st);
+    template <class I>
+    friend MySet<I> operator-(const MySet<I>& f_st, const MySet<I>& s_st);
+    template <class I>
+    friend MySet<I> operator*(const MySet<I>& f_st, const MySet<I>& s_st);
+};
+
+template<class INF>
+void MySet<INF>::add_element(INF el) {
+    if (!this->is_element(el)) {
+        MyVector<INF>::add_element(el);
         this->sort();
-        return true;
+    }
+    return;
+}
+
+template<class INF>
+void MySet<INF>::delete_element(INF el) {
+    int index = this->find(el);
+    if (index != -1) {
+        MyVector<INF>::delete_element(index);
+    }
+    return;
+}
+
+template<class INF>
+bool MySet<INF>::is_element(INF el)const  {
+    for (int i = 0; i < this->size_; ++i) {
+        if ((*this)[i] == el) {
+            return true;
+        }
     }
     return false;
-}
-
-template<typename INF>
-bool MySet<INF>::delete_element(INF el) {
-    if (is_element(el)) {
-        this->MyVector<INF>::delete_element(this->find(el));
-        return true;
-    }
-    return false;
-}
-
-template<typename INF>
-void MySet<INF>::operator+=(const MySet& St) {
-    for (size_t i = 0; i < St.len(); ++i) {
-        if (!is_element(St[i])) add_element(St[i]);
-    }
-}
-
-template<typename INF>
-void MySet<INF>::operator-=(const MySet& St) {
-    for (size_t i = 0; i < St.len(); ++i) {
-        if (is_element(St[i])) delete_element(St[i]);
-    }
-}
-
-template<typename INF>
-void MySet<INF>::operator*=(const MySet& St) {
-    for (size_t i = 0; i < this->len(); ++i) {
-        if (!St.is_element(this->operator[](i))) delete_element(this->operator[](i));
-    }
-}
-
-template<typename INF>
-MySet<INF> operator*(const MySet<INF>& f_st, const MySet<INF>& s_st) {
-    MySet<INF> new_v = f_st;
-    new_v *= s_st;
-    return new_v;
-}
-
-template<typename INF>
-MySet<INF> operator+(const MySet<INF>& f_st, const MySet<INF>& s_st) {
-    MySet<INF> new_v = f_st;
-    new_v += s_st;
-    return new_v;
-}
-
-template<typename INF>
-MySet<INF> operator-(const MySet<INF>& f_st, const MySet<INF>& s_st) {
-    MySet<INF> new_v = f_st;
-    new_v -= s_st;
-    return new_v;
-}
-
-template<typename INF>
-bool operator==(const MySet<INF>& f_st, const MySet<INF>& s_st) {
-    if (f_st.len() != s_st.len()) return false;
-    for (size_t i = 0; i < f_st.len(); ++i) {
-        if (f_st[i] != s_st[i]) return false;
-    }
-    return true;
 }
 
 template<>
-bool operator==(const MySet<char *>& f_st, const MySet<char *>& s_st) {
-    if (f_st.len() != s_st.len()) return false;
-    for (size_t i = 0; i < f_st.len(); ++i) {
-        if (std::strcmp(f_st[i], s_st[i]) != 0) return false;
+bool MySet<char*>::is_element(char* el) const{
+    for (int i = 0; i < this->size_; ++i) {
+        if (!strcmp((*this)[i], el)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template<class I>
+std::ostream& operator<<(std::ostream& out, const MySet<I>& Set) {
+    std::cout << "{";
+    for (int i = 0; i < Set.size_; ++i) {
+        if (i > 0) {
+            std::cout << ", ";
+        }
+        std::cout << Set[i];
+    }
+    std::cout << "}";
+    return out;
+}
+
+template<class I>
+bool operator==(const MySet<I>& f_st,const MySet<I>& s_st){
+    if (f_st.size_ != s_st.size_) {
+        return false;
+    }
+    for (int i = 0; i < f_st.size_; ++i) {
+        if (!(s_st.is_element(f_st[i]))) {
+            return false;
+        }
     }
     return true;
 }
 
-template<typename INF>
-std::ostream& operator<<(std::ostream& os, const MySet<INF>& St) {
-    for (size_t i = 0; i < St.len(); ++i) {
-        std::cout << St[i] << ' ';
+template<class I>
+MySet<I>& operator+=(MySet<I>& f_st,const MySet<I>& s_st) {
+    for (int i = 0; i < s_st.size_; ++i) {
+        f_st.add_element(s_st[i]);
     }
-    std::cout << std::endl;
-    return os;
+    return f_st;
 }
 
-#endif // MYSET_H
+template<class I>
+MySet<I>& operator-=( MySet<I>& f_st,const MySet<I>& s_st) {
+    for (int i = 0; i < f_st.size_; ++i) {
+        if (s_st.is_element((f_st[i]))) {
+            f_st.delete_element(f_st[i--]);
+        }
+    }
+    return f_st;
+}
+
+template<class I>
+MySet<I>& operator*=( MySet<I>& f_st,const MySet<I>& s_st) {
+    for (int i = 0; i < f_st.size_; ++i) {
+        if (!(s_st.is_element(f_st[i]))) {
+            f_st.delete_element(f_st[i--]);
+        }
+    }
+    return f_st;
+}
+
+template<class I>
+MySet<I> operator+(const MySet<I>& f_st, const MySet<I>& s_st) {
+    MySet<I> result = f_st;
+    result += s_st;
+    return result;
+}
+
+template<class I>
+MySet<I> operator-(const MySet<I>& f_st, const MySet<I>& s_st) {
+    MySet<I> result = f_st;
+    result -= s_st;
+    return result;
+}
+
+template<class I>
+MySet<I> operator*(const MySet<I>& f_st, const MySet<I>& s_st) {
+    MySet<I> result = f_st;
+    result *= s_st;
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -1,195 +1,37 @@
-#include <iostream>
 #include "MySet.h"
 
-// int main() {
-//     MySet<char*> set;
-
-//     // int choice, element;
-
-//     int choice;
-//     char element[100];
-
-//     std::cout << "Демонстрация работы MySet<int>" << std::endl;
-
-//     bool running = true;
-//     while(running) {
-//         std::cout << "\nТекущее множество: " << set;
-//         std::cout << "Меню:\n";
-//         std::cout << "1) Добавить элемент\n";
-//         std::cout << "2) Удалить элемент\n";
-//         std::cout << "3) Показать множество\n";
-//         std::cout << "0) Выход\n";
-//         std::cout << "Ваш выбор: ";
-//         std::cin >> choice;
-
-//         switch(choice) {
-//             case 1:
-//                 std::cin.ignore();
-//                 std::cout << "Введите элемент для добавления: ";
-//                 std::cin >> element;
-//                 // std::cout << "Введите элемент для добавления: ";
-
-//                 if (set.add_element(element)) {
-//                     std::cout << "Элемент " << element << " добавлен" << std::endl;
-//                 } else {
-//                     std::cout << "Элемент " << element << " уже в массиве" << std::endl;
-//                 }
-//                 // std::cout << "Введите элемент для добавления: ";
-
-//                 break;
-//             case 2:
-//                 std::cin.ignore();
-//                 std::cout << "Введите элемент для удаления: ";
-//                 std::cin >> element;
-//                 if (set.delete_element(element)) {
-//                     std::cout << "Элемент " << element << " удалён" << std::endl;
-//                 } else {
-//                     std::cout << "Элемент " << element << " не присутствовал в массиве" << std::endl;
-//                 }
-//                 break;
-//             case 3:
-//                 std::cin.ignore();
-//                 std::cout << "Множество: " << set;
-//                 break;
-//             case 0:
-//                 std::cin.ignore();
-//                 running = false;
-//                 break;
-//             default:
-//                 std::cin.ignore();
-//                 std::cout << "Неверный выбор. Повторите ввод." << std::endl;
-//                 break;
-//         }
-//     }
-//     std::cout << "Завершение работы программы." << std::endl;
-//     return 0;
-// }
-
-// #include <iostream>
-// #include "containers/myvector.h"
-// #include "containers/myset.h"
-
-
-
-
-// main.cpp
-// #include <iostream>
-// #include <clocale>
-// #include "MySet.h"
-
-// int main() {
-//     setlocale(LC_ALL, "Russian");
-
-//     MyVector<const char*> v;
-//     v.add_element("Привет!");
-//     v.add_element("Привет!");
-//     v.add_element("Привет!");
-//     v.add_element("Привет!");
-//     v.add_element("Привет!");
-//     std::cout << "Вектор v: " << v;
-//     v.add_element("Привет!");
-//     v.add_element("Привет!");
-//     v.add_element("Привет!");
-//     std::cout << "Вектор v: " << v;
-//     MyVector<const char*> v1 = v;
-//     std::cout << "Вектор v: " << v;
-
-//     v = v1;
-//     std::cout << "Вектор v1: " << v1;
-//     for (size_t i = 0; i < v.len(); ++i)
-//         v1.delete_element(0);
-//     std::cout << "Вектор v1: " << v1;
-//     std::cout << "Вектор v: " << v;
-
-    // MySet<const char*> s, s1, s2;
-    // s.add_element("Привет!");
-    // s.add_element("No");
-    // const char* str = "Hello!";
-    // s.add_element(str);
-    // std::cout << "Множество s: " << s;
-
-    // s1.add_element("Cat");
-    // s1.add_element("No");
-    // s1.add_element("Привет!");
-    // std::cout << "Множество s1: " << s1;
-
-    // s2 = s1 - s;
-    // std::cout << "Множество s2=s1-s: " << s2;
-    // std::cout << "Множество s1: " << s1;
-    // std::cout << "Множество s: " << s;
-
-    // s2 = s - s1;
-    // std::cout << "Множество s2=s-s1: " << s2;
-    // std::cout << "Множество s1: " << s1;
-    // std::cout << "Множество s: " << s;
-
-    // s2 = s1 + s;
-    // std::cout << "Множество s2=s1+s: " << s2;
-    // std::cout << "Множество s1: " << s1;
-    // std::cout << "Множество s: " << s;
-
-    // s2 = s1 * s;
-    // std::cout << "Множество s2=s1*s: " << s2;
-    // std::cout << "Множество s1: " << s1;
-    // std::cout << "Множество s: " << s;
-
-    // MySet<const char*> s3 = s2;
-    // std::cout << "Множество s3=s2: " << s3;
-
-    // if (s3 == s2) std::cout << "Множество s3=s2\n"; else std::cout << "Множество s3!=s2\n";
-    // if (s3 == s1) std::cout << "Множество s3=s1\n"; else std::cout << "Множество s3!=s1\n";
-    // if (s1 == s3) std::cout << "Множество s1=s3\n"; else std::cout << "Множество s1!=s3\n";
-
-//     return 0;
-// }
+const int MAX_SIZE = 128;
 
 int main() {
     setlocale(LC_ALL, "Russian");
 
-    MyVector<char*> v;
-    v.add_element((char*)"Привет!");
-    v.add_element((char*)"Привет!");
-    v.add_element((char*)"Привет!");
-    v.add_element((char*)"Привет!");
-    v.add_element((char*)"Привет!");
+    MyVector v("Hello!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
     std::cout << "Вектор v: " << v << std::endl;
-    v.add_element((char*)"Привет!");
-    v.add_element((char*)"Привет!");
-    v.add_element((char*)"Привет!");
-    v.delete_element(3);
+    v.add_element("Привет!");
+    v.add_element("Привет!");
+    v.add_element("Привет!");
     std::cout << "Вектор v: " << v << std::endl;
     MyVector v1 = v;
-    std::cout << "len v1: " << v1.len() << std::endl;
     std::cout << "Вектор v1: " << v1 << std::endl;
-    std::cout << "len v1: " << v1.len() << std::endl;
-    for (size_t i = 0; i < v.len(); i++){
-        std::cout << "len v1: " << v1.len() << std::endl;
-        std::cout << "len i: " << i << std::endl;
-        std::cout << "Вектор v: " << v;
-        std::cout << "Вектор v1: " << v1;
+    for (int i = 0; i < MAX_SIZE; i++)
         v1.delete_element(0);
-    }
-    std::cout << "len v: " << v.len() << std::endl;
-
     std::cout << "Вектор v1: " << v1 << std::endl;
-    std::cout << "Вектор v: " << v << std::endl;
-    MySet<char *> s, s1, s2;
-    s.add_element((char*)"Привет!");
-    s.add_element((char*)"No");
+    MySet s("Yes"), s1, s2;
+    s.add_element("Привет!");
+    s.add_element("No");
     char *str = (char*)"Hello!";
     s.add_element(str);
     std::cout << "Множество s: " << s << std::endl;
-    s1.add_element((char*)"Cat");
-    s1.add_element((char*)"No");
-    s1.add_element((char*)"Привет!");
+    s1.add_element("Cat");
+    s1.add_element("No");
+    s1.add_element("Привет!");
     std::cout << "Множество s1: " << s1 << std::endl;
-    std::cout << "\ndeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldel\n";
-
     s2 = s1 - s;
-    // s.delete_element((char*)"No");
-    std::cout << "\ndeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldeldel\n";
-    std::cout << s2[0];
-    s2.delete_element((char*)"Cat");
     std::cout << "Множество s2=s1-s: " << s2 << std::endl;
     std::cout << "Множество s1: " << s1 << std::endl;
     std::cout << "Множество s: " << s << std::endl;
@@ -219,7 +61,5 @@ int main() {
         std::cout << "Множество s1=s3\n";
     else
         std::cout << "Множество s1!=s3\n";
-
-
     return 0;
 }
